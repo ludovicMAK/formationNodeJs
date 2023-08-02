7682bc10b16a3dda89b462ca1a86a2ce4969a389
ludovicMAK <ludovic93mak@gmail.com>
2023-08-02 10:26:46 +0200

    installation des parseur minimist et argv et compréhension des commande

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ console.log("mon fichier a était modier");
 const fileContent = fs.readFileSync("test.txt","utf8");
 console.log(fileContent);*/
 
+
 /*const ft = require("./function");
 
 console.log(ft.multiply(5, 10))
@@ -24,9 +25,27 @@ console.log(chain(3)
     .multiply(2)
     .done()  )
 */
-const chalk = require("chalk");
+
+/*const chalk = require("chalk");
 
 console.log(chalk.bgRed("Erreur!"));
 console.log(chalk.green.bold("Succeès!"));
-console.log(chalk.yellow.underline("Avertissement!"));
+console.log(chalk.yellow.underline("Avertissement!")); */
+
+/*const  { argv } = require("process");
+
+const options = argv[2];
+
+if(options === "foo"){
+    console.log("foo");
+} else if(options === "bar"){
+    console.log("bar")
+}else {
+    console.log("default");
+}*/
+const argv = require("minimist")(process.argv.slice(2));
+
+console.log(argv);
+console.log(argv.name);
+console.log(argv.a);
 
