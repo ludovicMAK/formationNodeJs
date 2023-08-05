43313f4cb2f55c9d7de9141bd08bd204ecf628ac
ludovicMAK <ludovic93mak@gmail.com>
2023-08-05 19:04:29 +0200

    installation de express et création d'un serveur local

--- index.js
+++ index.js
@@ -124,28 +124,44 @@ console.log(jsonToObject.legal);
         
 //     });
 // calback:err,data
-const weather = (location,unit,callback)=>{
-    const url =`http://api.weatherstack.com/current?access_key=229639c46f875c77bd8a6e8157c6641c&query=${encodeURIComponent(location)}&units=${unit}`
-    fetch(url)
-        .then(res=>res.json())     
-        .then(data=>{
-            if(data.success === false){
-                callback(`Impossible de renvoyer vos information. Error ${data.error.code}: ${data.error.info}`,undefined)
-            }else{
-                const{current,location} = data
-            callback(undefined,`le nom de la ville est ${location.region}. le nom du pays est ${location.country}. Il fait ${current.temperature}°, le ressenti est de ${current.feelslike}. le temps est ${current.weather_descriptions[0]}`);
-            }
+// const weather = (location,unit,callback)=>{
+//     const url =`http://api.weatherstack.com/current?access_key=229639c46f875c77bd8a6e8157c6641c&query=${encodeURIComponent(location)}&units=${unit}`
+//     fetch(url)
+//         .then(res=>res.json())     
+//         .then(data=>{
+//             if(data.success === false){
+//                 callback(`Impossible de renvoyer vos information. Error ${data.error.code}: ${data.error.info}`,undefined)
+//             }else{
+//                 const{current,location} = data
+//             callback(undefined,`le nom de la ville est ${location.region}. le nom du pays est ${location.country}. Il fait ${current.temperature}°, le ressenti est de ${current.feelslike}. le temps est ${current.weather_descriptions[0]}`);
+//             }
             
-        });
-}
+//         });
+// }
 
-weather("","m", (err,data)=>{
-    console.log('Erreur:',err);
-    console.log('Data:', data);
+// weather("","m", (err,data)=>{
+//     console.log('Erreur:',err);
+//     console.log('Data:', data);
+// })
+
+// weather("New york","m", (err,data)=>{
+//     console.log('Erreur:',err);
+//     console.log('Data:', data);
+// })
+const express = require("express");
+const app = express();
+const port = 3000; //80
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello world</h1>");
+})
+app.get("/about",(req,res)=>{
+    res.send({
+        name:"alex",
+        age:30
+    });
 })
 
-weather("New york","m", (err,data)=>{
-    console.log('Erreur:',err);
-    console.log('Data:', data);
-})
-
+app.listen(port,()=>{
+    console.log(`App listening on port ${port}!`)
+});
