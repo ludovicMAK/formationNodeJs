2dfb312c25d1b4912ebd7b20bf439c55f53df096
ludovicMAK <ludovic93mak@gmail.com>
2023-08-08 18:32:06 +0200

    gérer les erreurs 404 et 500 avec Express

--- app.js
+++ app.js
@@ -175,6 +175,29 @@ app.get("/about",(req,res)=>{
         name:'alex'
     });
 })
+// app.use((req,res,next)=>{
+//     res.status(404).render('404',{
+//         title:'404',
+//     });
+// });
+
+// app.get("/about/*",(req,res)=>{
+//     res.render('404_about',{
+//          title:'404_about',
+//      });
+// })
+
+app.get("*",(req,res)=>{
+    res.render('404',{
+         title:'404',
+     });
+})
+
+app.use((err,req,res,next)=>{
+   console.error(err.stack);
+   res.status(500).send('Erreur,regardez le terminal!');
+});
+
 
 app.listen(port,()=>{
     console.log(`App listening on port ${port}!`)
